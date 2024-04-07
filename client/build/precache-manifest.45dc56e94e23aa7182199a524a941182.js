8dc12bb46cba4db2b2708854bec36f89051f2ddd
LerclercDuong <89032831+LerclercDuong@users.noreply.github.com>
2024-04-08 04:00:15 +0700

    build react

---
+++ client/build/precache-manifest.45dc56e94e23aa7182199a524a941182.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f175b6c0f46d1960c238cc24c760a95b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9c144be8a4c7a5ba52c",
+    "url": "/static/css/main.68ed5542.chunk.css"
+  },
+  {
+    "revision": "78eb283b174f94e7af04",
+    "url": "/static/js/2.966d86ca.chunk.js"
+  },
+  {
+    "revision": "00190af050e41e0a2e7b2ac492fe2d12",
+    "url": "/static/js/2.966d86ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba40fea22f2a60ddf405",
+    "url": "/static/js/3.37beed37.chunk.js"
+  },
+  {
+    "revision": "d9c144be8a4c7a5ba52c",
+    "url": "/static/js/main.34bc6623.chunk.js"
+  },
+  {
+    "revision": "207f9d74bfeaf4f32da8",
+    "url": "/static/js/runtime-main.ba360ab5.js"
+  },
+  {
+    "revision": "064ac3e9d262de2533dd4fce777cdf37",
+    "url": "/static/media/chatongptLogo.064ac3e9.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
